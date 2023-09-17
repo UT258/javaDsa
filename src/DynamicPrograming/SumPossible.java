8541829a6269e1cd91fc86999e9abb73d4da06ce
UT258 <utkarshsingh404499@gmail.com>
2023-09-17 19:27:45 +0530

    SumPossible dp is used now

--- src/DynamicPrograming/SumPossible.java
+++ src/DynamicPrograming/SumPossible.java
@@ -1,18 +1,19 @@
 package DynamicPrograming;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class SumPossible {
     public static void main(String[] args) {
         ArrayList <Integer>list=new ArrayList<>();
-        list.add(2);
-        list.add(1);
+       // list.add(1);
+        //list.add(2);
         list.add(3);
-        System.out.println(ispossible(4,list));
+        System.out.println(ispossible(25,list,new HashMap<Integer,Boolean>()));
 
     }
-    public static boolean ispossible(int n, List<Integer>list)
+    public static boolean ispossible(int n, List<Integer>list, HashMap<Integer,Boolean> map)
     {
         //base case
         if(n==0)
@@ -23,15 +24,19 @@ public class SumPossible {
         {
             return false;
         }
+        if (map.containsKey(n))
+        {
+            return map.get(n);
+        }
         for(int e:list)
         {
             int current =n-e;
-            if (ispossible(current,list))
-            {
+            if (ispossible(current,list,map))
+            {   map.put(n,true);
                 return true;
             }
         }
-
+      map.put(n,false);
         return false;
     }
 }
